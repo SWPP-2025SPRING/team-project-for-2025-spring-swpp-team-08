6dd1d69b8de028686ad52e24a98173a6c2419486
unknown <kdeun@snu.ac.kr>
2025-05-01 16:24:14 +0900

    UI expression changed

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -22,7 +22,7 @@ public class UIManager : MonoBehaviour
         int seconds = Mathf.FloorToInt(playtime % 60); 
         int milliseconds = Mathf.FloorToInt((playtime * 100) % 100); 
 
-        playtimeText.text = string.Format("{0:D2}:{1:D2}:{2:D2}", minutes, seconds, milliseconds);
+        playtimeText.text = string.Format("Playtime...{0:D2}:{1:D2}:{2:D2}", minutes, seconds, milliseconds);
     }
 
     //current stage play time
@@ -32,7 +32,7 @@ public class UIManager : MonoBehaviour
         int seconds = Mathf.FloorToInt(currentplaytime % 60);
         int milliseconds = Mathf.FloorToInt((currentplaytime * 100) % 100);
 
-        currentPlayTimeText.text = string.Format("{0:D2}:{1:D2}:{2:D2}", minutes, seconds, milliseconds);
+        currentPlayTimeText.text = string.Format("On current stage...{0:D2}:{1:D2}:{2:D2}", minutes, seconds, milliseconds);
     }
 
     public void UpdateStage(string stageName)
