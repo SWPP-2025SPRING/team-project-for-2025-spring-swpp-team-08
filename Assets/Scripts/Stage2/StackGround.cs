9dd2bd7e784de659c6ae412833aa0d8c65d91ef7
unknown <kdeun@snu.ac.kr>
2025-05-20 19:57:59 +0900

    added code for stack ground

---
+++ Assets/Scripts/Stage2/StackGround.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StackGround : MonoBehaviour
+{
+    public float disappearDelay = 0.5f; 
+    public bool destroyCompletely = true; 
+
+    private bool _triggered = false;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!_triggered && collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("collision");
+            _triggered = true;
+            Invoke(nameof(Disappear), disappearDelay);
+        }
+    }
+
+    void Disappear()
+    {
+        if (destroyCompletely)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
