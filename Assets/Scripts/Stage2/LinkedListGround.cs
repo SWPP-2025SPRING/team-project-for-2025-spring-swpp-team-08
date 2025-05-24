8dfaf1b3505b1e85e20f44f97caa788cca4d525f
unknown <kdeun@snu.ac.kr>
2025-05-24 19:43:17 +0900

    added namespace for convention

--- Assets/Scripts/Stage2/LinkedListGround.cs
+++ Assets/Scripts/Stage2/LinkedListGround.cs
@@ -2,51 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LinkedListGround : MonoBehaviour
+namespace Stage2
 {
-    public GameObject player; 
-    private PlayerController playerController; 
-    public int padNumber;
-    private static int LAST_PAD_NUM = 0;
-
-    private void Start()
+    public class LinkedListGround : MonoBehaviour
     {
-        if (player != null)
+        public GameObject player;
+        private PlayerController playerController;
+        public int padNumber;
+        private static int LAST_PAD_NUM = 0;
+
+        private void Start()
         {
-            playerController = player.GetComponent<PlayerController>();
-            if (playerController == null)
+            if (player != null)
             {
-                Debug.Log("PlayerController 컴포넌트를 찾을 수 없습니다!");
+                playerController = player.GetComponent<PlayerController>();
+                if (playerController == null)
+                {
+                    Debug.Log("PlayerController 컴포넌트를 찾을 수 없습니다!");
+                }
+            }
+            else
+            {
+                Debug.Log("Player 오브젝트가 연결되지 않았습니다!");
             }
         }
-        else
+
+        private void OnTriggerEnter(Collider other)
         {
-            Debug.Log("Player 오브젝트가 연결되지 않았습니다!");
-        }
-    }
+            if (!other.CompareTag("Player")) return;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (!other.CompareTag("Player")) return;
+            int expected = LAST_PAD_NUM + 1;
 
-        int expected = LAST_PAD_NUM + 1;
-
-        if (padNumber == expected)
-        {
-            Debug.Log($"순서대로 밟음! 현재: {padNumber}");
-            LAST_PAD_NUM = padNumber;
-        }
-        else
-        {
-            Debug.Log($"순서 틀림! 밟은 번호: {padNumber}, 기대한 번호: {expected}");
-            LAST_PAD_NUM = 0;
-
-            GameManager.Instance.playManager.DisplayCheckpointReturn();
-            Vector3 currentCheckPoint = GameManager.Instance.playManager.GetCurrentCheckpoint();
-
-            if (playerController != null)
+            if (padNumber == expected)
             {
-                playerController.MoveTo(currentCheckPoint);
+                Debug.Log($"순서대로 밟음! 현재: {padNumber}");
+                LAST_PAD_NUM = padNumber;
+            }
+            else
+            {
+                Debug.Log($"순서 틀림! 밟은 번호: {padNumber}, 기대한 번호: {expected}");
+                LAST_PAD_NUM = 0;
+
+                GameManager.Instance.playManager.DisplayCheckpointReturn();
+                Vector3 currentCheckPoint = GameManager.Instance.playManager.GetCurrentCheckpoint();
+
+                if (playerController != null)
+                {
+                    playerController.MoveTo(currentCheckPoint);
+                }
             }
         }
     }
