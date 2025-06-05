b302ae38d52eff01171ea2296f3e78853670bdcb
unknown <kdeun@snu.ac.kr>
2025-06-05 21:36:50 +0900

    changed checkpoint.cs

--- Assets/Scripts/Commons/Checkpoint.cs
+++ Assets/Scripts/Commons/Checkpoint.cs
@@ -5,24 +5,39 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    private static int INGAME_CHECKPOINT_NUM = 0;
     public int currentCheckpointNum;
     public Material triggeredMaterial;
     private bool _isTriggered = false;
+
+    private void Start()
+    {
+        CheckpointManager.Instance.RegisterCheckpoint(this); 
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !_isTriggered)
         {
             Vector3 checkpointPosition = transform.position;
             GameManager.Instance.playManager.UpdateCheckpoint(checkpointPosition);
-            _isTriggered=true;
-            INGAME_CHECKPOINT_NUM = currentCheckpointNum; // start from 1
             Debug.Log("Checkpoint saved at: " + checkpointPosition);
-            Renderer renderer = GetComponent<Renderer>();
-            if (renderer != null && triggeredMaterial != null)
-            {
-                renderer.material = triggeredMaterial;
-            }
+            usedCheckpoint();
+            CheckpointManager.Instance.DisablePreviousCheckpoints(currentCheckpointNum);
         }
+    }
+
+    private void usedCheckpoint()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        _isTriggered = true;
+        if (renderer != null && triggeredMaterial != null)
+        {
+            renderer.material = triggeredMaterial;
+        }
+    }
+
+    public void DisableCheckpoint()
+    {
+        usedCheckpoint();
     }
 }
---
+++ Assets/Scripts/Commons/CheckpointManager.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour
+{
+    public static CheckpointManager Instance { get; private set; }
+
+    private List<Checkpoint> allCheckpoints = new List<Checkpoint>();
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public void RegisterCheckpoint(Checkpoint checkpoint)
+    {
+        allCheckpoints.Add(checkpoint);
+    }
+
+    public void DisablePreviousCheckpoints(int currentNum)
+    {
+        foreach (Checkpoint cp in allCheckpoints)
+        {
+            if (cp.currentCheckpointNum < currentNum)
+            {
+                cp.DisableCheckpoint();
+            }
+        }
+    }
+
+    public void ResetAllCheckpoints()
+    {
+        foreach (Checkpoint cp in allCheckpoints)
+        {
+            cp.gameObject.SetActive(true);
+        }
+    }
+}
