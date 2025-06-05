623a7c42b524873791878104f298adb4aabac412
unknown <kdeun@snu.ac.kr>
2025-06-05 21:25:23 +0900

    changing code...ing 2

--- Assets/Scripts/Commons/Checkpoint.cs
+++ Assets/Scripts/Commons/Checkpoint.cs
@@ -7,15 +7,22 @@ public class Checkpoint : MonoBehaviour
 {
     private static int INGAME_CHECKPOINT_NUM = 0;
     public int currentCheckpointNum;
+    public Material triggeredMaterial;
     private bool _isTriggered = false;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && !_istriggered)
+        if (other.CompareTag("Player") && !_isTriggered)
         {
             Vector3 checkpointPosition = transform.position;
             GameManager.Instance.playManager.UpdateCheckpoint(checkpointPosition);
-            _istriggered=true;
+            _isTriggered=true;
+            INGAME_CHECKPOINT_NUM = currentCheckpointNum; // start from 1
             Debug.Log("Checkpoint saved at: " + checkpointPosition);
+            Renderer renderer = GetComponent<Renderer>();
+            if (renderer != null && triggeredMaterial != null)
+            {
+                renderer.material = triggeredMaterial;
+            }
         }
     }
 }
