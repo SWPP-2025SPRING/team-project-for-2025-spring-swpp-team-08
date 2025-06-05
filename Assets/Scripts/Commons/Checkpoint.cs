505eeeb95dc7faf4c869f9e4d244e7e315baa822
unknown <kdeun@snu.ac.kr>
2025-06-05 21:13:58 +0900

    changing code...ing

--- Assets/Scripts/Commons/Checkpoint.cs
+++ Assets/Scripts/Commons/Checkpoint.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    private static int INGAME_CHECKPOINT_NUM = 0;
+    public int currentCheckpointNum;
+    private bool _isTriggered = false;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !_istriggered)
         {
             Vector3 checkpointPosition = transform.position;
             GameManager.Instance.playManager.UpdateCheckpoint(checkpointPosition);
+            _istriggered=true;
             Debug.Log("Checkpoint saved at: " + checkpointPosition);
         }
     }
