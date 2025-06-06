bc9e56c8c4db908be4bf5ba4e849588e8208b07e
Sehyun Kim <mathshyun@gmail.com>
2025-06-06 18:04:31 +0900

    Add section 1-3

---
+++ Assets/Scripts/Stage4/ReverseMoveBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+namespace Stage4
+{
+    public class ReverseMoveBehaviour : PredefinedBehaviour
+    {
+        public float duration;
+
+        public Vector3 initialVelocity;
+        public Vector3 initialAngularVelocity;
+
+        private Vector3 _velocity;
+        private Vector3 _angularVelocity;
+
+        private void Awake()
+        {
+            Rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void Start()
+        {
+            _velocity = initialVelocity;
+            _angularVelocity = initialAngularVelocity;
+        }
+
+        private void FixedUpdate()
+        {
+            var deltaPosition = _velocity * Time.fixedDeltaTime;
+            Rigidbody.MovePosition(transform.position + deltaPosition);
+
+            var deltaAngle = Quaternion.Euler(_angularVelocity * (Time.fixedDeltaTime * Mathf.Deg2Rad));
+            Rigidbody.MoveRotation(Rigidbody.rotation * deltaAngle);
+        }
+
+        protected override void PerformInternal()
+        {
+            StartCoroutine(ReverseMoveCoroutine());
+            return;
+
+            IEnumerator ReverseMoveCoroutine()
+            {
+                var elapsedTime = 0f;
+
+                while (elapsedTime < duration)
+                {
+                    var progress = elapsedTime / duration;
+                    _velocity = initialVelocity * (1 - 2 * progress);
+                    _angularVelocity = initialAngularVelocity * (1 - 2 * progress);
+
+                    elapsedTime += Time.deltaTime;
+
+                    yield return null;
+                }
+
+                _velocity = -initialVelocity;
+                _angularVelocity = -initialAngularVelocity;
+            }
+        }
+    }
+}
