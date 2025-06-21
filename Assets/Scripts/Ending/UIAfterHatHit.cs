7a74ca48af4168f3f7a19212fd0e4686254251af
unknown <kdeun@snu.ac.kr>
2025-06-21 18:36:18 +0900

    apply refactoring

--- Assets/Scripts/Ending/UIAfterHatHit.cs
+++ Assets/Scripts/Ending/UIAfterHatHit.cs
@@ -24,18 +24,18 @@ public class UIAfterHatHit : MonoBehaviour
     {
         endingCreditPanel.SetActive(false);
 
-        string playerName = GameManager.Instance.GetCurrentPlayerName();
+        /*string playerName = GameManager.Instance.GetCurrentPlayerName();
         float score1 = GameManager.Instance.GetScores()[0];
         float score2 = GameManager.Instance.GetScores()[1];
         float score3 = GameManager.Instance.GetScores()[2];
         if (string.IsNullOrEmpty(playerName))
-            playerName = "Anonymous";
+            playerName = "Anonymous";*/
 
-        /*string playerName = "갯"; // 소수점 없이 정수 부분만 표시
+        string playerName = "갯"; // 소수점 없이 정수 부분만 표시
 
         float score1 = UnityEngine.Random.Range(10f, 100f);
         float score2 = UnityEngine.Random.Range(10f, 100f);
-        float score3 = UnityEngine.Random.Range(10f, 100f);*/
+        float score3 = UnityEngine.Random.Range(10f, 100f);
         SaveScore(playerName, score1, score2, score3);
 
         GameManager.Instance.LoadScene("LeaderboardMyRankScene");
---
+++ Assets/Scripts/ScoreBoard/ShowTopNNew.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShowTopNNew : MonoBehaviour
+{
+    [Header("Top 5 Ranks UI")]
+    public List<TextMeshProUGUI> rankTexts;
+    public List<TextMeshProUGUI> nameTexts;
+    public List<TextMeshProUGUI> timeTexts;
+
+    public int topN = 5;
+    private string _filePath;
+
+    private void Awake()
+    {
+        _filePath = ScoreBoardHelper.GetScoreboardFilePath();
+    }
+
+    private void Start()
+    {
+        ShowTopScores(topN);
+    }
+
+    private void ShowTopScores(int count)
+    {
+        List<ScoreBoardHelper.ScoreData> scores = ScoreBoardHelper.LoadScores(_filePath);
+        scores.Sort((a, b) => a.score.CompareTo(b.score));
+        ScoreBoardHelper.ShowScores(scores.GetRange(0, Mathf.Min(count, scores.Count)), 0, rankTexts, nameTexts, timeTexts, count);
+    }
+}
