7cdb0339bb8521f6e32742f6ec602e7e6835a9c2
trz-21 <rlaxodbs0515@gmail.com>
2025-06-22 14:28:43 +0900

    input name ui default로 가리기

--- Assets/Scripts/Opening/OpeningManager.cs
+++ Assets/Scripts/Opening/OpeningManager.cs
@@ -15,6 +15,7 @@ public class OpeningManager : MonoBehaviour
     private IEnumerator PlayOpening()
     {
         yield return openingSequence.PlaySequence();
+        playerNameInput.gameObject.SetActive(true);
         playerNameInput.Show();
     }
 
