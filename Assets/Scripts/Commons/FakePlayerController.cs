36b530690523cb1543a4cb0276f66c778650d057
Sehyun Kim <mathshyun@gmail.com>
2025-06-22 22:44:46 +0900

    Modify main scene background

---
+++ Assets/Scripts/Commons/FakePlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class FakePlayerController : NewPlayerControl
+{
+    protected new void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    protected new void Update()
+    {
+        return;
+    }
+
+    protected new void FixedUpdate()
+    {
+        return;
+    }
+
+    public new void Jump(float magnitude)
+    {
+        rb.AddForce(Vector3.up * magnitude, ForceMode.Impulse);
+    }
+}
