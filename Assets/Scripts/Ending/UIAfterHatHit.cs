b507049b915055dfd35d30acb7985017eb0ab0e2
unknown <kdeun@snu.ac.kr>
2025-06-23 03:21:29 +0900

    sbmanager

--- Assets/Scripts/Ending/UIAfterHatHit.cs
+++ Assets/Scripts/Ending/UIAfterHatHit.cs
@@ -31,13 +31,13 @@ public class UIAfterHatHit : MonoBehaviour
         if (string.IsNullOrEmpty(playerName))
             playerName = "Anonymous";*/
 
-       string playerName = "갯"; // 소수점 없이 정수 부분만 표시
+        string playerName = "갯2"; // 소수점 없이 정수 부분만 표시
 
         float score1 = UnityEngine.Random.Range(10f, 100f);
         float score2 = UnityEngine.Random.Range(10f, 100f);
         float score3 = UnityEngine.Random.Range(10f, 100f);
         SaveScore(playerName, score1, score2, score3);
-
+        Debug.Log("saved22");
         GameManager.Instance.LoadScene("LeaderboardMyRankScene");
     }
 
