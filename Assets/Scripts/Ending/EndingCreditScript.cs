8c092b19d54ef834f79d6fb02a5231e805737122
Sehyun Kim <mathshyun@gmail.com>
2025-06-23 12:30:18 +0900

    Modify ending credit duration

--- Assets/Scripts/Ending/EndingCreditScript.cs
+++ Assets/Scripts/Ending/EndingCreditScript.cs
@@ -4,7 +4,7 @@ public class EndingCreditScript : MonoBehaviour
 {
     public RectTransform creditText;
     public float scrollSpeed = 30f;
-    public float endY = 450f; 
+    public float endY;
     public System.Action onCreditEnd;
 
     private bool _isRolling = false;
@@ -12,7 +12,6 @@ public class EndingCreditScript : MonoBehaviour
     public void StartRoll()
     {
         _isRolling = true;
-        creditText.anchoredPosition = new Vector2(0, -100f); 
     }
 
     void Update()
@@ -24,7 +23,7 @@ public class EndingCreditScript : MonoBehaviour
         if (creditText.anchoredPosition.y >= endY)
         {
             _isRolling = false;
-            onCreditEnd?.Invoke(); 
+            onCreditEnd?.Invoke();
         }
     }
 }
